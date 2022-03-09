bad54cbd8de1b59713fbaed32b2be71e00523023
everton10info <everton10info@gmail.com>
2022-03-09 19:49:55 -0300

    Atualização

--- lib/models/transaction_model.dart
+++ lib/models/transaction_model.dart
@@ -5,12 +5,14 @@ class TransactionM {
   String nameTransaction;
   DateTime date = DateTime.now();
   String typeTransaction;
+  DateTime dueDate;
   double valor;
 
   TransactionM({
     required this.id,
     required this.nameTransaction,
     date,
+    required this.dueDate,
     required this.typeTransaction,
     required this.valor,
   });
@@ -19,6 +21,7 @@ class TransactionM {
         'id': id,
         'nameTransaction': nameTransaction,
         'date': date.toString(),
+        'dueDate': dueDate.toString(),
         'typeTransaction': typeTransaction,
         'valor': valor
       };
